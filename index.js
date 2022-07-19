a5460fd1a29bc332bca372496b327ba1dd5c6b73
Afiq Izzan <afiqizzan16@gmail.com>
2022-07-19 17:02:52 +0800

    c

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const app = express();
-
 const mysql = require('mysql');
 const cors = require('cors');
+
+const app = express();
 app.use(cors());
 
 var connect = mysql.createPool({
