3bd37f392d6ed0b15ffa08828c94329150b6a176
Afiq Izzan <afiqizzan16@gmail.com>
2022-07-19 22:22:08 +0800

    com

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
     res.send('Salam Muafakat!');
 });
 
-app.get('/johor', (req, res) => {
+app.get('/listevent', (req, res) => {
     var micro_username = req.query.username;
 
   console.log("username: " + micro_username);
@@ -35,6 +35,25 @@ app.get('/johor', (req, res) => {
       });
     }
   });
+});
+app.get('/listpeople', (req, res) => {
+  var micro_username = req.query.username;
+
+console.log("username: " + micro_username);
+
+connect.getConnection(function (err, connection) {
+  if (err) { res.send('Error Database Connection'); }
+  else {
+    var sql = "SELECT * FROM people";
+    connect.query(sql, function (err, result) {
+      if (err) { throw err; }
+      else {
+        res.send(result);
+      }
+    connection.release();
+    });
+  }
+});
 });
 
 app.listen(process.env.PORT, ()=> {
