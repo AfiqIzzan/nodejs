afd2e865d3f53b818681275de8532a10d4145011
Afiq Izzan <afiqizzan16@gmail.com>
2022-07-19 16:06:50 +0800

    third commit

--- index.js
+++ index.js
@@ -6,7 +6,23 @@ app.get('/', (req, res) => {
 });
 
 app.get('/johor', (req, res) => {
-    res.json({ data: "Salam Muafakat" });
+    var micro_username = req.query.username;
+
+  console.log("username: " + micro_username);
+
+  connect.getConnection(function (err, connection) {
+    if (err) { res.send('Error Database Connection'); }
+    else {
+      var sql = "SELECT * FROM mosquemanagement";
+      connect.query(sql, function (err, result) {
+        if (err) { throw err; }
+        else {
+          res.send(result);
+        }
+      connection.release();
+      });
+    }
+  });
 });
 
 app.listen(8000, () => {
